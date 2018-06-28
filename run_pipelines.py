bca0c6d33dc026baf7bf51c28d97faa79a185be1
Scott White <whitews@gmail.com>
2018-06-28 10:30:52 -0400

    update run_pipelines, also added caching for training / testing

--- run_pipelines.py
+++ run_pipelines.py
@@ -1,6 +1,18 @@
+import os
+import pickle
 from pipelines.engineered_metrics_svm.pipeline import SVMPipeline
 
-eng_svm = SVMPipeline('data/image_set_73')
-eng_svm.train()
-eng_svm.test()
-pass
+if not os.path.isdir('tmp'):
+    os.mkdir('tmp')
+
+try:
+    eng_svm = pickle.load(open('tmp/73_SVMPipeline.pkl', 'rb'))
+except (FileNotFoundError, TypeError):
+    eng_svm = SVMPipeline('data/image_set_73')
+    eng_svm.train()
+    eng_svm.test()
+    fh = open('tmp/73_SVMPipeline.pkl', 'wb')
+    pickle.dump(eng_svm, fh)
+    fh.close()
+
+report = eng_svm.report()
