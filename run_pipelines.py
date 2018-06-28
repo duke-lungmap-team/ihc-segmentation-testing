cc359b78e35c8b3716f719a9786f7c464b836cd9
Scott White <whitews@gmail.com>
2018-06-28 19:26:41 -0400

    kinda crazy dynamic importing and running of Pipeline subclasses

--- run_pipelines.py
+++ run_pipelines.py
@@ -1,18 +1,44 @@
 import os
 import pickle
-from pipelines.engineered_metrics_svm.pipeline import SVMPipeline
+import pkgutil
+from importlib import import_module
+import inspect
+from pipelines.base_pipeline import Pipeline
 
+# dynamically load all sub-classes of our base Pipeline class
+pipelines = {}
+
+for (ff, name, is_pkg) in pkgutil.iter_modules(['pipelines']):
+    if name in ['base_pipeline', 'common_utils']:
+        continue
+
+    new_module = import_module('pipelines.' + name, package='pipelines')
+
+    for i in dir(new_module):
+        attribute = getattr(new_module, i)
+        if inspect.isclass(attribute) and issubclass(attribute, Pipeline):
+            pipelines[i] = attribute
+
+# get list of image sets
+image_set_dirs = os.listdir('data')
+
+# make our 'tmp' directory for caching trained & tested pipeline instances
 if not os.path.isdir('tmp'):
     os.mkdir('tmp')
 
-try:
-    eng_svm = pickle.load(open('tmp/73_SVMPipeline.pkl', 'rb'))
-except (FileNotFoundError, TypeError):
-    eng_svm = SVMPipeline('data/image_set_73')
-    eng_svm.train()
-    eng_svm.test()
-    fh = open('tmp/73_SVMPipeline.pkl', 'wb')
-    pickle.dump(eng_svm, fh)
-    fh.close()
+for image_set in image_set_dirs:
+    for pipe_class_name, pipe_class in pipelines.items():
+        pkl_path = os.path.join('tmp', '_'.join([image_set, pipe_class_name + '.pkl']))
+        image_set_path = os.path.join('data', image_set)
 
-report = eng_svm.report()
+        try:
+            pipe_instance = pickle.load(open(pkl_path, 'rb'))
+        except (FileNotFoundError, TypeError):
+            pipe_instance = pipe_class(image_set_path)
+            pipe_instance.train()
+            pipe_instance.test()
+            fh = open(pkl_path, 'wb')
+            pickle.dump(pipe_instance, fh)
+            fh.close()
+
+        report = pipe_instance.report()
