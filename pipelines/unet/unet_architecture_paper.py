b1fe772690d1c8b429a84cbb7477b558d1c4a819
benneely <nigelneely@gmail.com>
2018-11-30 11:08:56 -0500

    including a new color unet architecture and PEP8 cleanup of some older architectures.

--- pipelines/unet/unet_architecture_paper.py
+++ pipelines/unet/unet_architecture_paper.py
@@ -1,51 +1,111 @@
-from keras.models import *
 from keras.layers import *
-from keras.optimizers import *
-from pipelines.unet.loss import weighted_categorical_crossentropy
+from keras.models import Model
+from keras.layers import Input, BatchNormalization, Dropout
+from keras.layers.convolutional import Conv2D, Conv2DTranspose
+from keras.layers.pooling import MaxPooling2D
+from keras.layers.merge import concatenate
+from keras.optimizers import Adam
+
+
+def color_model(input_shape=(1024,1024)):
+    # Build U-Net model
+    inputs = Input(input_shape+(3,))
+    s = BatchNormalization()(inputs) # we can learn the normalization step
+    s = Dropout(0.5)(s)
+
+    c1 = Conv2D(8, (3, 3), activation='relu', padding='same')(s)
+    c1 = Conv2D(8, (3, 3), activation='relu', padding='same')(c1)
+    p1 = MaxPooling2D((2, 2))(c1)
+
+    c2 = Conv2D(16, (3, 3), activation='relu', padding='same')(p1)
+    c2 = Conv2D(16, (3, 3), activation='relu', padding='same')(c2)
+    p2 = MaxPooling2D((2, 2))(c2)
+
+    c3 = Conv2D(32, (3, 3), activation='relu', padding='same')(p2)
+    c3 = Conv2D(32, (3, 3), activation='relu', padding='same')(c3)
+    p3 = MaxPooling2D((2, 2))(c3)
+
+    c4 = Conv2D(64, (3, 3), activation='relu', padding='same')(p3)
+    c4 = Conv2D(64, (3, 3), activation='relu', padding='same')(c4)
+    p4 = MaxPooling2D(pool_size=(2, 2))(c4)
+
+    c5 = Conv2D(128, (3, 3), activation='relu', padding='same')(p4)
+    c5 = Conv2D(128, (3, 3), activation='relu', padding='same')(c5)
+
+    u6 = Conv2DTranspose(64, (2, 2), strides=(2, 2), padding='same')(c5)
+    u6 = concatenate([u6, c4])
+    c6 = Conv2D(64, (3, 3), activation='relu', padding='same')(u6)
+    c6 = Conv2D(64, (3, 3), activation='relu', padding='same')(c6)
+
+    u7 = Conv2DTranspose(32, (2, 2), strides=(2, 2), padding='same')(c6)
+    u7 = concatenate([u7, c3])
+    c7 = Conv2D(32, (3, 3), activation='relu', padding='same')(u7)
+    c7 = Conv2D(32, (3, 3), activation='relu', padding='same')(c7)
+
+    u8 = Conv2DTranspose(16, (2, 2), strides=(2, 2), padding='same')(c7)
+    u8 = concatenate([u8, c2])
+    c8 = Conv2D(16, (3, 3), activation='relu', padding='same')(u8)
+    c8 = Conv2D(16, (3, 3), activation='relu', padding='same')(c8)
+
+    u9 = Conv2DTranspose(8, (2, 2), strides=(2, 2), padding='same')(c8)
+    u9 = concatenate([u9, c1], axis=3)
+    c9 = Conv2D(8, (3, 3), activation='relu', padding='same')(u9)
+    c9 = Conv2D(8, (3, 3), activation='relu', padding='same')(c9)
+
+    outputs = Conv2D(1, (1, 1), activation='sigmoid')(c9)
+
+    model = Model(inputs=[inputs], outputs=[outputs])
+    model.compile(optimizer=Adam(lr=1e-4), loss='binary_crossentropy', metrics=['accuracy'])
+    return model
+
 
 def unet(class_num, img_rows=572, img_cols=572, channels=3):
     inputs = Input((img_rows, img_cols, channels))
     conv1 = Conv2D(64, 3, activation='relu', padding='valid', kernel_initializer='he_normal')(inputs)
     conv1 = Conv2D(64, 3, activation='relu', padding='valid', kernel_initializer='he_normal')(conv1)
-    crop1 = Cropping2D(cropping=((88,88),(88,88)))(conv1)
+    crop1 = Cropping2D(cropping=((88, 88), (88, 88)))(conv1)
     pool1 = MaxPooling2D(pool_size=(2, 2))(conv1)
 
     conv2 = Conv2D(128, 3, activation='relu', padding='valid', kernel_initializer='he_normal')(pool1)
     conv2 = Conv2D(128, 3, activation='relu', padding='valid', kernel_initializer='he_normal')(conv2)
-    crop2 = Cropping2D(cropping=((40,40),(40,40)))(conv2)
+    crop2 = Cropping2D(cropping=((40, 40), (40, 40)))(conv2)
     pool2 = MaxPooling2D(pool_size=(2, 2))(conv2)
 
     conv3 = Conv2D(256, 3, activation='relu', padding='valid', kernel_initializer='he_normal')(pool2)
     conv3 = Conv2D(256, 3, activation='relu', padding='valid', kernel_initializer='he_normal')(conv3)
-    crop3 = Cropping2D(cropping=((15,17),(15,17)))(conv3)
+    crop3 = Cropping2D(cropping=((15, 17), (15, 17)))(conv3)
     pool3 = MaxPooling2D(pool_size=(2, 2))(conv3)
 
     conv4 = Conv2D(512, 3, activation='relu', padding='valid', kernel_initializer='he_normal')(pool3)
     conv4 = Conv2D(512, 3, activation='relu', padding='valid', kernel_initializer='he_normal')(conv4)
     drop4 = Dropout(0.5)(conv4)
-    crop4 = Cropping2D(cropping=((4,4),(4,4)))(drop4)
+    crop4 = Cropping2D(cropping=((4, 4), (4, 4)))(drop4)
     pool4 = MaxPooling2D(pool_size=(2, 2))(drop4)
 
     conv5 = Conv2D(1024, 3, activation='relu', padding='valid', kernel_initializer='he_normal')(pool4)
     conv5 = Conv2D(1024, 3, activation='relu', padding='valid', kernel_initializer='he_normal')(conv5)
     drop5 = Dropout(0.5)(conv5)
 
-    up6 = Conv2D(512, 2, activation='relu', padding='same', kernel_initializer='he_normal')(UpSampling2D(size=(2, 2))(drop5))
+    up6 = Conv2D(512, 2, activation='relu', padding='same', kernel_initializer='he_normal')\
+        (UpSampling2D(size=(2, 2))(drop5))
     merge6 = concatenate([crop4, up6], axis=3)
     conv6 = Conv2D(512, 3, activation='relu', padding='valid', kernel_initializer='he_normal')(merge6)
     conv6 = Conv2D(512, 3, activation='relu', padding='valid', kernel_initializer='he_normal')(conv6)
 
-    up7 = Conv2D(256, 2, activation='relu', padding='same', kernel_initializer='he_normal')(UpSampling2D(size=(2, 2))(conv6))
+    up7 = Conv2D(256, 2, activation='relu', padding='same', kernel_initializer='he_normal')\
+        (UpSampling2D(size=(2, 2))(conv6))
     merge7 = concatenate([crop3, up7], axis=3)
     conv7 = Conv2D(256, 3, activation='relu', padding='valid', kernel_initializer='he_normal')(merge7)
     conv7 = Conv2D(256, 3, activation='relu', padding='valid', kernel_initializer='he_normal')(conv7)
 
-    up8 = Conv2D(128, 2, activation='relu', padding='same', kernel_initializer='he_normal')(UpSampling2D(size=(2, 2))(conv7))
+    up8 = Conv2D(128, 2, activation='relu', padding='same', kernel_initializer='he_normal')\
+        (UpSampling2D(size=(2, 2))(conv7))
     merge8 = concatenate([crop2, up8], axis=3)
     conv8 = Conv2D(128, 3, activation='relu', padding='valid', kernel_initializer='he_normal')(merge8)
     conv8 = Conv2D(128, 3, activation='relu', padding='valid', kernel_initializer='he_normal')(conv8)
 
-    up9 = Conv2D(64, 2, activation='relu', padding='same', kernel_initializer='he_normal')(UpSampling2D(size=(2, 2))(conv8))
+    up9 = Conv2D(64, 2, activation='relu', padding='same', kernel_initializer='he_normal')\
+        (UpSampling2D(size=(2, 2))(conv8))
     merge9 = concatenate([crop1, up9], axis=3)
     conv9 = Conv2D(64, 3, activation='relu', padding='valid', kernel_initializer='he_normal')(merge9)
     conv9 = Conv2D(64, 3, activation='relu', padding='valid', kernel_initializer='he_normal')(conv9)
@@ -61,47 +121,51 @@ def unet_padded(class_num, img_rows=1024, img_cols=1024, channels=3):
     #try dialating the kernal skips to gain more information
     conv1 = Conv2D(64, 3, activation='relu', padding='same', kernel_initializer='he_normal')(inputs)
     conv1 = Conv2D(64, 3, activation='relu', padding='same', kernel_initializer='he_normal')(conv1)
-    # crop1 = Cropping2D(cropping=((6,6),(6,6)))(conv1)
+    # crop1 = Cropping2D(cropping=((6, 6), (6, 6)))(conv1)
     pool1 = MaxPooling2D(pool_size=(2, 2))(conv1)
 
     conv2 = Conv2D(128, 3, activation='relu', padding='same', kernel_initializer='he_normal')(pool1)
     #take off activate
     #Activation -> Batchnormilization
     conv2 = Conv2D(128, 3, activation='relu', padding='same', kernel_initializer='he_normal')(conv2)
-    # crop2 = Cropping2D(cropping=((3,3),(3,3)))(conv2)
+    # crop2 = Cropping2D(cropping=((3, 3), (3, 3)))(conv2)
     pool2 = MaxPooling2D(pool_size=(2, 2))(conv2)
 
     conv3 = Conv2D(256, 3, activation='relu', padding='same', kernel_initializer='he_normal')(pool2)
     conv3 = Conv2D(256, 3, activation='relu', padding='same', kernel_initializer='he_normal')(conv3)
-    # crop3 = Cropping2D(cropping=((2,1),(2,1)))(conv3)
+    # crop3 = Cropping2D(cropping=((2, 1), (2, 1)))(conv3)
     pool3 = MaxPooling2D(pool_size=(2, 2))(conv3)
 
     conv4 = Conv2D(512, 3, activation='relu', padding='same', kernel_initializer='he_normal')(pool3)
     conv4 = Conv2D(512, 3, activation='relu', padding='same', kernel_initializer='he_normal')(conv4)
     drop4 = Dropout(0.5)(conv4)
-    # crop4 = Cropping2D(cropping=((0,0),(0,0)))(drop4)
+    # crop4 = Cropping2D(cropping=((0, 0), (0, 0)))(drop4)
     pool4 = MaxPooling2D(pool_size=(2, 2))(drop4)
 
     conv5 = Conv2D(1024, 3, activation='relu', padding='same', kernel_initializer='he_normal')(pool4)
     conv5 = Conv2D(1024, 3, activation='relu', padding='same', kernel_initializer='he_normal')(conv5)
     drop5 = Dropout(0.5)(conv5)
     #try conv2dtranspose
-    up6 = Conv2D(512, 2, activation='relu', padding='same', kernel_initializer='he_normal')(UpSampling2D(size=(2, 2))(drop5))
+    up6 = Conv2D(512, 2, activation='relu', padding='same', kernel_initializer='he_normal')\
+        (UpSampling2D(size=(2, 2))(drop5))
     merge6 = concatenate([drop4, up6], axis=3)
     conv6 = Conv2D(512, 3, activation='relu', padding='same', kernel_initializer='he_normal')(merge6)
     conv6 = Conv2D(512, 3, activation='relu', padding='same', kernel_initializer='he_normal')(conv6)
 
-    up7 = Conv2D(256, 2, activation='relu', padding='same', kernel_initializer='he_normal')(UpSampling2D(size=(2, 2))(conv6))
+    up7 = Conv2D(256, 2, activation='relu', padding='same', kernel_initializer='he_normal')\
+        (UpSampling2D(size=(2, 2))(conv6))
     merge7 = concatenate([conv3, up7], axis=3)
     conv7 = Conv2D(256, 3, activation='relu', padding='same', kernel_initializer='he_normal')(merge7)
     conv7 = Conv2D(256, 3, activation='relu', padding='same', kernel_initializer='he_normal')(conv7)
 
-    up8 = Conv2D(128, 2, activation='relu', padding='same', kernel_initializer='he_normal')(UpSampling2D(size=(2, 2))(conv7))
+    up8 = Conv2D(128, 2, activation='relu', padding='same', kernel_initializer='he_normal')\
+        (UpSampling2D(size=(2, 2))(conv7))
     merge8 = concatenate([conv2, up8], axis=3)
     conv8 = Conv2D(128, 3, activation='relu', padding='same', kernel_initializer='he_normal')(merge8)
     conv8 = Conv2D(128, 3, activation='relu', padding='same', kernel_initializer='he_normal')(conv8)
 
-    up9 = Conv2D(64, 2, activation='relu', padding='same', kernel_initializer='he_normal')(UpSampling2D(size=(2, 2))(conv8))
+    up9 = Conv2D(64, 2, activation='relu', padding='same', kernel_initializer='he_normal')\
+        (UpSampling2D(size=(2, 2))(conv8))
     merge9 = concatenate([conv1, up9], axis=3)
     conv9 = Conv2D(64, 3, activation='relu', padding='same', kernel_initializer='he_normal')(merge9)
     conv9 = Conv2D(64, 3, activation='relu', padding='same', kernel_initializer='he_normal')(conv9)
